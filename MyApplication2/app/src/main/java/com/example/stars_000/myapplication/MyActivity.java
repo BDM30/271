421bc3361f6607f7f84954d6f64c840d7fe610e5
Egor <gumin.egor@gmail.com>
2014-11-02 16:25:27 +0400

    Добавлен неработающий хттп-запрос!

--- MyApplication2/app/src/main/java/com/example/stars_000/myapplication/MyActivity.java
+++ MyApplication2/app/src/main/java/com/example/stars_000/myapplication/MyActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,6 +24,7 @@ public class MyActivity extends Activity implements View.OnClickListener {
     //private Socket client;
     private EditText myText;
     private Button send;
+    Button http_btn;
     private TextView myTextView;
     Button gotosignpage;
     ///
@@ -39,6 +44,9 @@ public class MyActivity extends Activity implements View.OnClickListener {
 
         send = (Button) findViewById(R.id.button);
         send.setText("Send");
+
+        http_btn = (Button) findViewById(R.id.http_btn);
+        http_btn.setOnClickListener(this);
 
         send.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -66,6 +74,9 @@ public class MyActivity extends Activity implements View.OnClickListener {
             case R.id.gotosignpage:
                 Intent intent = new Intent(this, Sign_in.class);
                 startActivity(intent);
+                break;
+            case R.id.http_btn:
+                c.getHTML("http://192.168.10.108:11000/lol");
                 break;
             default:
                 break;
