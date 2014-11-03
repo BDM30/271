a8cd11abb23a4eff972be40f541d8cef8b839ce1
bdm30 <starson4587@gmail.com>
2014-11-03 12:32:40 +0300

    1
    1

---
+++ работает серв/SimpleHttpServer/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace myServer {
+
+    // запускаем главный класс HttpServer. Слушает клиентов и оправляет их обрабочику = HttpProcessor. Обрабочик принимает
+    // клиента, а также указатель на класс сервера, чтобы вызывать POST и GET методы.
+    public class MyMain {
+        public static int Main(String[] args) {
+            HttpServer httpServer = new MyHttpServer(11000);
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+            return 0;
+        }
+
+    }
+
+}
+
+
+
