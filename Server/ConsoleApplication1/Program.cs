945fb6278d7769d6ec4a96c7b79baa0c2742e0c0
bdm30 <starson4587@gmail.com>
2014-11-03 13:32:45 +0300

    Сервер
    работает с внешней сети

---
+++ Server/ConsoleApplication1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        private static string POST(string Url, string Data)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(Url);
+            req.Method = "POST";
+            req.Timeout = 100000;
+            req.ContentType = "application/x-www-form-urlencoded";
+            byte[] sentData = Encoding.GetEncoding(1251).GetBytes(Data);
+            req.ContentLength = sentData.Length;
+            System.IO.Stream sendStream = req.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
+            System.Net.WebResponse res = req.GetResponse();
+            System.IO.Stream ReceiveStream = res.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8);
+            //Кодировка указывается в зависимости от кодировки ответа сервера
+            Char[] read = new Char[256];
+            int count = sr.Read(read, 0, 256);
+            string Out = String.Empty;
+            while (count > 0)
+            {
+                String str = new String(read, 0, count);
+                Out += str;
+                count = sr.Read(read, 0, 256);
+            }
+            return Out;
+        }
+
+        private static string GET(string Url, string Data)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(Url + "?" + Data);
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.Stream stream = resp.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            string Out = sr.ReadToEnd();
+            sr.Close();
+            return Out;
+        }
+
+        static void Main(string[] args)
+        {
+            //string Answer = POST("http://localhost:8080/", "hello");
+            //Console.WriteLine(Answer);
+            //Answer = Mamatov Andrey
+            string answer = GET("http://localhost:11000/", "func=remind;email=starson4503@yandex.ru;");
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Server/SimpleHttpServer/HttpServer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace myServer
+{
+    public abstract class HttpServer
+    {
+
+        protected int port;
+        TcpListener listener;
+        bool is_active = true;
+        ConsoleManager consoleManager;
+
+        public HttpServer(int port)
+        {
+            this.port = port;
+            consoleManager = new ConsoleManager();
+        }
+
+        public void listen()
+        {
+            listener = new TcpListener(port);
+            listener.Start();
+            while (is_active)
+            {
+                TcpClient s = listener.AcceptTcpClient();
+                consoleManager.playSoynds();
+                consoleManager.printDate();
+                HttpProcessor processor = new HttpProcessor(s, this);
+                Thread thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+                Thread.Sleep(1);
+            }
+        }
+
+        public abstract void handleGETRequest(HttpProcessor p);
+    }
+
+    public class MyHttpServer : HttpServer
+    {
+        public MyHttpServer(int port)
+            : base(port)
+        {
+        }
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            Console.WriteLine("request: {0}", p.http_url);
+            string query = (p.http_url).Trim(new Char[] { '/' });
+            Console.WriteLine("q = {0}", query);
+            string answer = "defaut value";
+
+            //
+            // Вызвали базу пользователей
+            UserBase userBase = new UserBase();
+            userBase.loadFile(); // Загрузили имеющихся пользователей
+
+            // убрал точку с запятой с запроса. Это необходимо для дальнейшей обрабоки
+            query = query.Substring(0, query.Length - 1);
+
+
+
+            // завели разделители, которыми будем парсить
+            char[] charSeparatorsBlocks = new char[] { ';' };
+            char[] charSeparatorsNameValue = new char[] { '=' };
+
+            // получили массив блоков. То, что находится между ';'.
+            string[] arrayBlocks = query.Split(charSeparatorsBlocks, StringSplitOptions.None);
+
+
+            // Парсим запрос
+            switch (arrayBlocks.Length)
+            {
+                // случай регистрации и входа
+                case 3:
+                    // Получили массив из 2х элементов. В виде ключ = значение.
+                    string[] arrayNameValueMethod = arrayBlocks[0].Split(charSeparatorsNameValue, StringSplitOptions.None);
+                    string[] arrayNameValueEmail = arrayBlocks[1].Split(charSeparatorsNameValue, StringSplitOptions.None);
+                    string[] arrayNameValuePassword = arrayBlocks[2].Split(charSeparatorsNameValue, StringSplitOptions.None);
+
+                    // управление фукциями
+                    switch (arrayNameValueMethod[1])
+                    {
+                        case "registration":
+                            answer = "func=registration;";
+                            User found = userBase.existUser(arrayNameValueEmail[1]);
+                            if (found == null)
+                            {
+                                userBase.addUser(arrayNameValueEmail[1], arrayNameValuePassword[1]);
+                                userBase.saveFile();
+                                answer += "result=1;";
+                            }
+                            else
+                                answer += "result=0;";
+                            break;
+                        case "entrace":
+                            Console.WriteLine("entrace!");
+                            answer = "func=entace;";
+                            if (userBase.existUser(arrayNameValueEmail[1], arrayNameValuePassword[1]))
+                                answer += "result=1;";
+                            else
+                                answer += "result=0;";
+                            break;
+
+                    }
+                    break;
+
+                case 2:
+                    string[] arrayNameValueMethod2 = arrayBlocks[0].Split(charSeparatorsNameValue, StringSplitOptions.None);
+                    string[] arrayNameValueEmail2 = arrayBlocks[1].Split(charSeparatorsNameValue, StringSplitOptions.None);
+                    switch (arrayNameValueMethod2[1])
+                    {
+                        case "remind":
+                            Console.WriteLine("remind!");
+                            answer = "func=remind;";
+                            if (userBase.remindPassword(arrayNameValueEmail2[1]))
+                                answer += "result=1;";
+                            else
+                                answer += "result=0;";
+                            break;
+                    }
+                    break;
+                default:
+                    answer = "wrong query";
+                    break;
+
+            }
+            //
+
+
+            // сюда встатить код обработки get запросов
+            p.writeSuccess();
+            p.outputStream.WriteLine(answer);
+        }
+    }
+}
