8e7411589cf67afd8b0e6110e900d24ff2ec89c3
Egor <colored.lime@gmail.com>
2014-12-05 03:44:47 +0400

    На тестовую кнопку http прикрутил json. далее прикручу везде его и нормальный асинк

--- Lymnologic/src/com/geo/Lymnologic/MyActivity.java
+++ Lymnologic/src/com/geo/Lymnologic/MyActivity.java
@@ -2,28 +2,25 @@ package com.geo.Lymnologic;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 
 public class MyActivity extends Activity implements View.OnClickListener {
 
-    //private Socket client;
     private EditText myText;
     private Button send;
     Button http_btn;
     public String lol;
-    TextView myTextView;
     Button gotosignpage;
-    ///тест 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +30,15 @@ public class MyActivity extends Activity implements View.OnClickListener {
         myText = (EditText) findViewById(R.id.editText);
         myText.setText("type smth");
 
-        myTextView = (TextView) findViewById(R.id.textView);
-
         send = (Button) findViewById(R.id.button);
         send.setText("Send");
 
         http_btn = (Button) findViewById(R.id.http_btn);
         http_btn.setOnClickListener(this);
+
+        gotosignpage = (Button) findViewById(R.id.gotosignpage);
+        gotosignpage.setOnClickListener(this);
+
 
         send.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -54,17 +53,11 @@ public class MyActivity extends Activity implements View.OnClickListener {
                     e.printStackTrace();
                 }
                 myText.setText(""); //Очищаем наше поле ввода
-                myTextView.setText(sndtest);
+                Toast.makeText(getBaseContext(), sndtest, Toast.LENGTH_LONG).show();
             }
         });
-
-//Переходим на страницу регистрации
-        gotosignpage = (Button) findViewById(R.id.gotosignpage);
-        gotosignpage.setOnClickListener(this);
-
     }
 
-    //Это наш активити-листенер. Скоро мы будем обрабатывать нажатие любой кнопки тут.
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -75,10 +68,12 @@ public class MyActivity extends Activity implements View.OnClickListener {
                 break;
 
             case R.id.http_btn:
+
+                myText.setText("тык");
                 //Пример http-запроса
-                Request.HttpAsyncTask test = new Request.HttpAsyncTask();
-                test.execute(Request.serverIP + "func=registration;email=starson450@yandex.ru;password=vlad;");
-                try {
+                //Register test = new Register();
+                //test.execute(Request.serverIP + "func=registration;email=starson450@yandex.ru;password=vlad;");
+                /*try {
                     lol = test.get(10, TimeUnit.SECONDS);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -88,11 +83,52 @@ public class MyActivity extends Activity implements View.OnClickListener {
                     Toast.makeText(this, "Сервер не отвечает.", Toast.LENGTH_LONG).show();
                     e.printStackTrace();
                 }
-                myTextView.setText(lol);
+                Toast.makeText(this, lol, Toast.LENGTH_LONG).show();*/
+                JSONObject reg_query = new JSONObject();
+                try {
+                    reg_query .put("function", "registration");
+                    //reg_query .put("email", email.getText().toString());
+                    //reg_query .put("password", pass.getText().toString());
+                    reg_query .put("email", "test2@mail.com");
+                    reg_query .put("password", "test");
+                }
+                catch(JSONException ex) {
+                    ex.printStackTrace();
+                }
+                String allQuery = Request.serverIP +  Uri.encode(reg_query.toString());
+                Toast.makeText(this, allQuery, Toast.LENGTH_LONG).show();
+                new Register().execute(allQuery);
                 break;
 
             default:
                 break;
         }
     }
+
+    public class Register extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return Request.GET(urls[0]);
+        }
+        protected void onPostExecute(String res) {
+            String method = "";
+            String answer = "";
+            try {
+                JSONObject dataJsonObj = new JSONObject(res);
+                method = (dataJsonObj.getString("function"));
+                answer = (dataJsonObj.getString("result"));
+                myText.setText(method + answer);
+                if ("registration;1".equals(method + ";" + answer)) {
+                    Intent intent = new Intent(getBaseContext(), Notifications.class);
+                    startActivity(intent);
+                }
+
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+
+
+            }
+        }
+    }
 }
+
