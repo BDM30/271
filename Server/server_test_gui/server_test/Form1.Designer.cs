ef51297c319ab34903c61b1704558b1e66415917
bdm30 <starson4587@gmail.com>
2014-12-09 18:39:05 +0300

    271: added testing gui tool

---
+++ Server/server_test_gui/server_test/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace server_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_version_api = new System.Windows.Forms.Label();
+            this.label_email = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_x = new System.Windows.Forms.Label();
+            this.label_y = new System.Windows.Forms.Label();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.textBox_y = new System.Windows.Forms.TextBox();
+            this.button_registration = new System.Windows.Forms.Button();
+            this.button_entrance = new System.Windows.Forms.Button();
+            this.button_remind = new System.Windows.Forms.Button();
+            this.button_add_note = new System.Windows.Forms.Button();
+            this.button_get_note = new System.Windows.Forms.Button();
+            this.label_ip = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.label_result = new System.Windows.Forms.Label();
+            this.richTextBox_result = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label_version_api
+            // 
+            this.label_version_api.AutoSize = true;
+            this.label_version_api.Location = new System.Drawing.Point(23, 9);
+            this.label_version_api.Name = "label_version_api";
+            this.label_version_api.Size = new System.Drawing.Size(46, 17);
+            this.label_version_api.TabIndex = 0;
+            this.label_version_api.Text = "label1";
+            // 
+            // label_email
+            // 
+            this.label_email.AutoSize = true;
+            this.label_email.Location = new System.Drawing.Point(23, 38);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(46, 17);
+            this.label_email.TabIndex = 1;
+            this.label_email.Text = "label1";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(23, 69);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(46, 17);
+            this.label_password.TabIndex = 2;
+            this.label_password.Text = "label1";
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Location = new System.Drawing.Point(434, 41);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(46, 17);
+            this.label_name.TabIndex = 3;
+            this.label_name.Text = "label1";
+            // 
+            // label_x
+            // 
+            this.label_x.AutoSize = true;
+            this.label_x.Location = new System.Drawing.Point(616, 41);
+            this.label_x.Name = "label_x";
+            this.label_x.Size = new System.Drawing.Size(46, 17);
+            this.label_x.TabIndex = 4;
+            this.label_x.Text = "label1";
+            // 
+            // label_y
+            // 
+            this.label_y.AutoSize = true;
+            this.label_y.Location = new System.Drawing.Point(616, 69);
+            this.label_y.Name = "label_y";
+            this.label_y.Size = new System.Drawing.Size(46, 17);
+            this.label_y.TabIndex = 5;
+            this.label_y.Text = "label1";
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(100, 41);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(268, 22);
+            this.textBox_email.TabIndex = 6;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(100, 69);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(268, 22);
+            this.textBox_password.TabIndex = 7;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(486, 41);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(100, 22);
+            this.textBox_name.TabIndex = 8;
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(668, 41);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(100, 22);
+            this.textBox_x.TabIndex = 9;
+            // 
+            // textBox_y
+            // 
+            this.textBox_y.Location = new System.Drawing.Point(668, 69);
+            this.textBox_y.Name = "textBox_y";
+            this.textBox_y.Size = new System.Drawing.Size(100, 22);
+            this.textBox_y.TabIndex = 10;
+            // 
+            // button_registration
+            // 
+            this.button_registration.Location = new System.Drawing.Point(26, 132);
+            this.button_registration.Name = "button_registration";
+            this.button_registration.Size = new System.Drawing.Size(180, 23);
+            this.button_registration.TabIndex = 11;
+            this.button_registration.Text = "button1";
+            this.button_registration.UseVisualStyleBackColor = true;
+            this.button_registration.Click += new System.EventHandler(this.button_registration_Click);
+            // 
+            // button_entrance
+            // 
+            this.button_entrance.Location = new System.Drawing.Point(26, 161);
+            this.button_entrance.Name = "button_entrance";
+            this.button_entrance.Size = new System.Drawing.Size(180, 23);
+            this.button_entrance.TabIndex = 12;
+            this.button_entrance.Text = "button1";
+            this.button_entrance.UseVisualStyleBackColor = true;
+            this.button_entrance.Click += new System.EventHandler(this.button_entrance_Click);
+            // 
+            // button_remind
+            // 
+            this.button_remind.Location = new System.Drawing.Point(26, 190);
+            this.button_remind.Name = "button_remind";
+            this.button_remind.Size = new System.Drawing.Size(180, 23);
+            this.button_remind.TabIndex = 13;
+            this.button_remind.Text = "button1";
+            this.button_remind.UseVisualStyleBackColor = true;
+            this.button_remind.Click += new System.EventHandler(this.button_remind_Click);
+            // 
+            // button_add_note
+            // 
+            this.button_add_note.Location = new System.Drawing.Point(268, 132);
+            this.button_add_note.Name = "button_add_note";
+            this.button_add_note.Size = new System.Drawing.Size(212, 23);
+            this.button_add_note.TabIndex = 14;
+            this.button_add_note.Text = "button1";
+            this.button_add_note.UseVisualStyleBackColor = true;
+            this.button_add_note.Click += new System.EventHandler(this.button_add_note_Click);
+            // 
+            // button_get_note
+            // 
+            this.button_get_note.Location = new System.Drawing.Point(268, 161);
+            this.button_get_note.Name = "button_get_note";
+            this.button_get_note.Size = new System.Drawing.Size(212, 23);
+            this.button_get_note.TabIndex = 15;
+            this.button_get_note.Text = "button1";
+            this.button_get_note.UseVisualStyleBackColor = true;
+            this.button_get_note.Click += new System.EventHandler(this.button_get_note_Click);
+            // 
+            // label_ip
+            // 
+            this.label_ip.AutoSize = true;
+            this.label_ip.Location = new System.Drawing.Point(433, 8);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(46, 17);
+            this.label_ip.TabIndex = 16;
+            this.label_ip.Text = "label1";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(486, 6);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(282, 22);
+            this.textBox_ip.TabIndex = 17;
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Location = new System.Drawing.Point(235, 216);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(46, 17);
+            this.label_result.TabIndex = 18;
+            this.label_result.Text = "label1";
+            // 
+            // richTextBox_result
+            // 
+            this.richTextBox_result.Location = new System.Drawing.Point(39, 236);
+            this.richTextBox_result.Name = "richTextBox_result";
+            this.richTextBox_result.Size = new System.Drawing.Size(503, 115);
+            this.richTextBox_result.TabIndex = 19;
+            this.richTextBox_result.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 385);
+            this.Controls.Add(this.richTextBox_result);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label_ip);
+            this.Controls.Add(this.button_get_note);
+            this.Controls.Add(this.button_add_note);
+            this.Controls.Add(this.button_remind);
+            this.Controls.Add(this.button_entrance);
+            this.Controls.Add(this.button_registration);
+            this.Controls.Add(this.textBox_y);
+            this.Controls.Add(this.textBox_x);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.label_y);
+            this.Controls.Add(this.label_x);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_email);
+            this.Controls.Add(this.label_version_api);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_version_api;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_x;
+        private System.Windows.Forms.Label label_y;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.TextBox textBox_y;
+        private System.Windows.Forms.Button button_registration;
+        private System.Windows.Forms.Button button_entrance;
+        private System.Windows.Forms.Button button_remind;
+        private System.Windows.Forms.Button button_add_note;
+        private System.Windows.Forms.Button button_get_note;
+        private System.Windows.Forms.Label label_ip;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.RichTextBox richTextBox_result;
+    }
+}
+
---
+++ Server/server_test_gui/server_test/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label_version_api.Text = "Api version 0.4";
+            label_email.Text = "email";
+            label_password.Text = "password";
+            label_name.Text = "name";
+            label_x.Text = "x";
+            label_y.Text = "y";
+            label_ip.Text = "ip";
+            label_result.Text = "result";
+
+            textBox_email.Text = "starson4587@gmail.com";
+            textBox_password.Text = "koolherc";
+            textBox_name.Text = "test1";
+            textBox_x.Text = "3";
+            textBox_y.Text = "4";
+            textBox_ip.Text = "http://localhost:11000/";
+
+            button_registration.Text = "registration";
+            button_entrance.Text = "entrance";
+            button_remind.Text = "remind";
+            button_add_note.Text = "add note";
+            button_get_note.Text = "get notes";
+        }
+
+        private void button_get_note_Click(object sender, EventArgs e)
+        {
+            richTextBox_result.Text = "";
+            string begin = textBox_ip.Text;
+            string query = "{\"function\":\"get_notes\",\"email\":\"" + textBox_email.Text + "\"}";
+            richTextBox_result.AppendText("Query:\n" + begin + query);
+            richTextBox_result.AppendText("\nAnswer:\n" + QuerySender.GET(begin, query));
+        }
+
+        private void button_registration_Click(object sender, EventArgs e)
+        {
+            richTextBox_result.Text = "";
+            string begin = textBox_ip.Text;
+            string query = "{\"function\":\"registration\",\"email\":\"" + textBox_email.Text + "\",\"password\":\"" + textBox_password.Text + "\"}";
+            richTextBox_result.AppendText("Query:\n"+begin + query);
+            richTextBox_result.AppendText("\nAnswer:\n" + QuerySender.GET(begin, query));
+        }
+
+        private void button_entrance_Click(object sender, EventArgs e)
+        {
+            richTextBox_result.Text = "";
+            string begin = textBox_ip.Text;
+            string query = "{\"function\":\"entrance\",\"email\":\"" + textBox_email.Text + "\",\"password\":\"" + textBox_password.Text + "\"}";
+            richTextBox_result.AppendText("Query:\n" + begin + query);
+            richTextBox_result.AppendText("\nAnswer:\n" + QuerySender.GET(begin, query));
+        }
+
+        private void button_remind_Click(object sender, EventArgs e)
+        {
+            richTextBox_result.Text = "";
+            string begin = textBox_ip.Text;
+            string query = "{\"function\":\"remind\",\"email\":\"" + textBox_email.Text + "\"}";
+            richTextBox_result.AppendText("Query:\n" + begin + query);
+            richTextBox_result.AppendText("\nAnswer:\n" + QuerySender.GET(begin, query));
+        }
+
+        private void button_add_note_Click(object sender, EventArgs e)
+        {
+            richTextBox_result.Text = "";
+            string begin = textBox_ip.Text;
+
+            string query = "{\"function\":\"add_note\",\"name\":\"" + textBox_name.Text +
+                "\",\"user\":\"" + textBox_email.Text + "\",\"x\":" + textBox_x.Text + ",\"y\":" + textBox_y.Text + "}";
+            richTextBox_result.AppendText("Query:\n" + begin + query);
+            richTextBox_result.AppendText("\nAnswer:\n" + QuerySender.GET(begin, query));
+        }
+    }
+}
