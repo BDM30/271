8ba20619ab24b4c38916d7b388f48a2afdf20ea1
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-14 22:48:49 +0300

    - Добавил Ninject и EF. - Добавил сущность User.

--- Food/API/Controllers/UserController.cs
+++ Food/API/Controllers/UserController.cs
@@ -1,16 +1,30 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 using System.Web.WebPages;
+using Domain.Abstract;
+using Domain.Entities;
 
 namespace API.Controllers
 {
     public class UserController : ApiController
     {
 
-    [HttpGet]
-    public string Test (int x)
-    {
-      return x.ToString();
-    }
+      private ICommonRepository<User> userRepository;
+
+      public UserController(ICommonRepository<User> users)
+      {
+        userRepository = users;
+      }
+
+      [Route("test")]
+      [HttpGet]
+      public User Test()
+      {
+        User x = new User() {Name = "Vlad", Password = "Pass"};
+        userRepository.SaveData(x);
+        User y = userRepository.Data.FirstOrDefault();
+        return y;
+      }
 
   }
 }
---
+++ Food/Domain/Concrete/FoodContext.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Класс для связи Entity Framework с баззой данных
+ * содержит коллекцию User
+ */
+using System.Data.Entity;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+  public class FoodContext : DbContext
+  {
+    public DbSet<User> Users { get; set; }
+
+  }
+}
