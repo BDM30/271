7f1201330b8cf86ba6b7eb421096c995ce6774b2
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-14 22:09:35 +0300

    Создал пустое приложение - которое будет серваком.

---
+++ Food/API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.WebPages;
+
+namespace API.Controllers
+{
+    public class UserController : ApiController
+    {
+
+    [HttpGet]
+    public string Test (int x)
+    {
+      return x.ToString();
+    }
+
+  }
+}
