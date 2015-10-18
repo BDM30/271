78ca680efe108967506483e8f5453ee71519a93b
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-18 15:48:12 +0300

    Добавил тестовую функцию

--- Food/API/Controllers/HomeController.cs
+++ Food/API/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using Domain.Entities;
 
 namespace API.Controllers
 {
@@ -15,12 +10,5 @@ namespace API.Controllers
         {
             return View();
         }
-
-      [System.Web.Http.HttpGet]
-      [System.Web.Mvc.Route("Test/get")]
-      public string getTest(string id)
-      {
-        return id == "4606068035723" ?  "Гирлянда" : "nope";
-      }
   }
 }
---
+++ Food/API/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using API.Models;
+
+namespace API.Controllers
+{
+  public class TestController : ApiController
+  {
+    public NameDescription one(string id)
+    {
+      return id == "4606068035723" ? new NameDescription() {Description = "Супер-гирлянда", Name = "Гирлянда" } : null;
+    }
+  }
+}
